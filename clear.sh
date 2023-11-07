084cc9c879f57b609d4286e95224523f1ce4aabe
Ninad Tashildar <tashildarnd@gmail.com>
2023-11-07 16:01:17 -0700

    Uploading project from local folder

---
+++ clear.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+
+rm BCHOC_FILE_PATH
+
+./bchoc init
---
+++ test.sh
@@ -0,0 +1,26 @@
+./bchoc init
+
+./bchoc init
+
+./bchoc verify
+
+./bchoc add -c 65cc391d-6568-4dcc-a3f1-86a2f04140f3 -i 987654321 -i 123456789
+
+./bchoc add -c 65cc391d65684dcca3f186a2f04140f3 -i 987654322
+
+./bchoc add -c 65cc391d-6568-4dcc-a3f1-86a2f04140f3 -i 123456788
+
+./bchoc checkout -i 987654321
+
+./bchoc checkout -i 987654321
+
+./bchoc checkin -i 987654321
+
+./bchoc log -r -n 2 -i 987654321
+
+./bchoc remove -i 987654321 -y RELEASED -o "John Doe, 123 Cherry Ln, Pleasant, AZ 84848, 480-XXX-4321"
+
+
+
+
+
